ae5b9ab44df05cbe9fb481c6cc3f09145ab43a1b
AndreyPodvigin <sn.podvigin.andrey@gmail.com>
2022-06-27 12:48:37 +0300

    first attempt

--- src/index.js
+++ src/index.js
@@ -11,4 +11,48 @@ fastify.register(import('@fastify/multipart'), {
 });
 fastify.register(import('@fastify/cookie'));
 
+
+fastify.post('/uppercase', (request, reply) => {
+  const string = request.body.toUpperCase();
+  if (string.includes('FUCK')) {
+    return reply.status(403).send('unresolved')
+  } else {
+    return reply.status(200).send(string);
+  }
+});
+
+fastify.post('/lowercase', (request, reply) => {
+  const string = request.body.toLowerCase();
+  if (string.includes('fuck')) {
+    return reply.status(403).send('unresolved')
+  } else {
+    return reply.status(200).send(string);
+  }
+});
+
+fastify.get('/user/:id', (request, reply) => {
+  const userID = users[request.params.id];
+  if (userID) {
+    return reply.status(200).send(userID);
+  } else {
+    return reply.status(400).send('User not exist');
+  }
+})
+
+fastify.get('/users', (request, reply) => {
+  const usersArray = Object.values(users);
+  let { filter, value } = request.query;
+  if (filter === undefined || value === undefined) {
+    return reply.status(200).send(usersArray);
+  } else {
+    const filteredUsers = usersArray.filter((item) => {
+      if (item[filter].toString() === value) {
+        return item;
+      }
+    });
+    return reply.status(200).send(filteredUsers);
+  }
+
+})
+
 export default fastify;
